4b66b64a343e051f79fd01c6d5057f54f201569c
RATI RANJAN KAR <9564716+kratiranjan@users.noreply.github.com>
2018-05-18 17:12:41 +0530

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,7 +17,7 @@ $message = $_POST['message'];
 $to = 'kratiranjan@outlook.com'; 
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@outlook.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@outlook.com\n"; // This is the email address the generated message will be from. Ex. noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
